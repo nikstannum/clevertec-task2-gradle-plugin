f88f3e141bda0c3f26e9a60aa8f470a4c5635333
Nikita Semeniuk <nikstannum@yandex.ru>
2023-10-13 23:19:15 +0300

    dev: cleanup code

--- plugin/src/main/groovy/ru/clevertec/ReportPlugin.groovy
+++ plugin/src/main/groovy/ru/clevertec/ReportPlugin.groovy
@@ -1,7 +1,7 @@
 package ru.clevertec
 
-import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.Project
 import org.gradle.api.tasks.testing.TestReport
 
 class ReportPlugin implements Plugin<Project> {
@@ -22,14 +22,10 @@ class ReportPlugin implements Plugin<Project> {
         }
 
         project.task('report', type: TestReport) {
-            doFirst {
-                println('**do first**')
-            }
             dependsOn('test')
             destinationDir = project.file("${project.buildDir}/reports/mytest")
-            println('----STARTS----')
             def tR = getTestResults()
-            tR.forEach {println it}
+            tR.forEach { println it }
             reportOn(project.getTasks().named('test').get())
         }
     }
